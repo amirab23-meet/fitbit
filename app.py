04a00762b14615d64dd522c8c44b837886c8a007
amirab23-meet <109903948+amirab23-meet@users.noreply.github.com>
2022-09-01 06:34:17 +0300

    almost done  i swear

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
+
 import pyrebase
+
+num = 2
 
 config = {
   "apiKey": "AIzaSyBwMLMG6TlrTRWvrImpn7kgKxVEF3ExJRI",
@@ -10,8 +13,24 @@ config = {
   "messagingSenderId": "531475169244",
   "appId": "1:531475169244:web:06192ca20d569bc89a5919",
   "measurementId": "G-SL0HLVBCQG",
-  "databaseURL":"https://cs-amir-gp-f-default-rtdb.europe-west1.firebasedatabase.app/"
-};
+  "databaseURL":"https://cs-amir-gp-f-default-rtdb.europe-west1.firebasedatabase.app/"};
+
+watch = {
+  1: "Charge 5",
+  2: "Versa 3",
+  3: "Charge 3",
+  4: "Versa 1",
+  5: "VivoFit 4",
+  6: "Inspire 2",
+  7: "FitBit Mi 3",
+  8: "FitBit Mi 4",
+  9: "Pink Inspire 3",
+  10: "ForeRunner 23",
+  11: "VivoFit 3",
+  12: "VivoSmart 99",
+  13: "Luxury Venue 2"
+
+}
 
 
 firebase = pyrebase.initialize_app(config)
@@ -26,7 +45,7 @@ app.config['SECRET_KEY'] = 'trytoguessbish'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    error=""
+    error="failed"
     if request.method == "POST":
         login_session['email'] = request.form['email']
         login_session['password'] = request.form['password']
@@ -35,9 +54,9 @@ def signin():
             return(redirect('design'))
         except:
             error="problem"
+            return render_template("inerror.html")
+            
     return render_template("signin.html")
-
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -48,10 +67,12 @@ def signup():
         login_session['username']= request.form['username']
         login_session['location']= request.form['location']
         login_session['password']= request.form['password']
+        login_session['card']= request.form['card']
+
 
         # try:
         login_session['user'] = auth.create_user_with_email_and_password(login_session["email"], request.form['password'])
-        user= {"email": request.form['email'],"full_name": request.form['full_name'],"username": request.form['username'],"location": request.form['location'],"password": request.form['password'] }
+        user= {"email": request.form['email'],"full_name": request.form['full_name'],"username": request.form['username'],"location": request.form['location'],"password": request.form['password'], "card": request.form['card'] }
         user = db.child("Users").child(login_session['user']['localId']).set(user)
         return redirect(url_for('design'))
         # except:
@@ -82,7 +103,9 @@ def features():
 
 @app.route('/shop')
 def shop():
-    return render_template("shop.html")
+    
+    return render_template("shop.html" ,   )
+
 
 
 @app.route('/abouts')
@@ -91,7 +114,9 @@ def abouts():
 
 @app.route('/cart')
 def cart():
-    return render_template("cart.html")
+   
+
+    return render_template("cart.html" ,watch=watch)
 
 @app.route('/ship')
 def ship():
@@ -101,12 +126,21 @@ def ship():
         
     full_name = db.child("Users").child(login_session['user']['localId']).get().val()['full_name']
     location = db.child("Users").child(login_session['user']['localId']).get().val()['location']
+    card = db.child("Users").child(login_session['user']['localId']).get().val()['card']
     
-    return render_template("ship.html" , location=location , full_name = full_name ,)
+        
+    
+    return render_template("ship.html" , location=location , full_name = full_name ,card=card)
 
 
 
 
+
+
+
+@app.route('/inerror')
+def inerror():
+    return render_template("inerror.html")
 
 
 
